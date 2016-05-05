1ffca59b5ac27bd077f51fc4383392f07c86a725
SBRKS <sbrks3@gmail.com>
2016-05-05 11:27:45 -0700

    homework through q5

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,47 +1,105 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x, y) {
+  if (x > y) {
+  	return x;
+  }
+  else {
+  	return y;
+  }
 }
 
 // Question 2
-function maxOfThree() {
+function maxOfThree(x, y, z) {
+	if (x > y && x > z) {
+		return x;
+	}
+	else if (y > x && y > z) {
+		return y;
+	}
 
+	else {
+		return z;
+	}
 }
+
+
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(char) {
+	var vowels = [a, e, i o, u];
+	if (charr)
 
+	else if (char === a, e, i, o, u) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(array) {
+	var total = 0;
+
+  for (var i=0; sum = 0; i<array.length; i++) {
+  	total += sum[i];
+  }
+
+  return total;
 }
 
+  // take in numbers
+  // iterate through and add all numbers in the array
+  // total the numbers
+  // print the total
 
-// Question 4
-function multiplyArray() {
+function multiplyArray(array) {
 
+	var product = arr[0];
+
+	for (var i=0; i<array.length; i++) {
+		product *= array[i];
+	}
+
+	return product;
 }
+
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+ 	return args.lengths;
 }
 
 
 
 // Question 6
-var reverseString = function (){
+var reverseString = function (arr){
+	return arr.reverse();
   
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (arr) {
+	var longest = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].length > longest) {
+
+		}
+	}
 }
+
+function findLongestWord (arr) {
+  longest = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > longest) {
+      longest = arr[i].length;
+    }
+  }
+  return longest;
+}
+
 
 
 // Question 8
